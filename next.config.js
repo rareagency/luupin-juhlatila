164f476de0ea5861997e54622cdfd86a43124693
Loviisa Hurme <loviisa.hurme@outlook.com>
2022-04-13 16:45:09 +0300

    Add calendar

--- next.config.js
+++ next.config.js
@@ -6,11 +6,23 @@ const nextConfig = {
   },
 };
 
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid",
+  "@fullcalendar/google-calendar",
+]);
+
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins(
   [
+    withTM,
     [
       optimizedImages,
       {
